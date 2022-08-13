4c0b7af738a77b13d14306a8b870056973cb5180
Michal Caban <cabanmichal@gmail.com>
2022-08-13 19:04:30 +0200

    initial commit - first two days

---
+++ aoc2020_01.py
@@ -0,0 +1,59 @@
+from typing import List
+import bisect
+import math
+
+
+INPUT = "aoc2020_01_input.txt"
+SUM = 2020
+
+
+def load_entries(report_file: str) -> List[int]:
+    with open(report_file, "rt", encoding="utf-8") as infile:
+        return list(map(int, infile.readlines()))
+
+
+def number_in_list(numbers: List[int], number: int, start: int = 0) -> bool:
+    idx = bisect.bisect_left(numbers, number, lo=start)
+    return idx != len(numbers) and numbers[idx] == number
+
+
+def find_n_entries(numbers: List[int], n: int, total: int) -> List[int]:
+    if n < 1:
+        raise ValueError("n should be 1 or higher")
+
+    selected: List[int] = []
+
+    def find(start: int = 0) -> bool:
+        if len(selected) == n - 1:
+            remaining = total - sum(selected)
+            if number_in_list(numbers, remaining, start):
+                selected.append(remaining)
+                return True
+            return False
+
+        for idx, number in enumerate(numbers[start:]):
+            selected.append(number)
+            completed = find(start=start + idx + 1)
+            if completed:
+                return True
+            selected.pop()
+
+        return False
+
+    find()
+
+    return selected
+
+
+def main() -> None:
+    numbers = sorted(load_entries(report_file=INPUT))
+
+    entries = find_n_entries(numbers, 2, SUM)
+    print(entries, math.prod(entries))
+
+    entries = find_n_entries(numbers, 3, SUM)
+    print(entries, math.prod(entries))
+
+
+if __name__ == "__main__":
+    main()
---
+++ aoc2020_02.py
@@ -0,0 +1,60 @@
+from dataclasses import dataclass
+from typing import Type, TypeVar
+import re
+
+T = TypeVar("T", bound="Record")
+INPUT = "aoc2020_02_input.txt"
+
+_pattern = re.compile(r"(\d+)-(\d+) (.): (.+)")
+
+
+@dataclass
+class Record:
+    min_count: int
+    max_count: int
+    character: str
+    password: str
+
+    @classmethod
+    def from_password_list_line(cls: Type[T], line: str) -> T:
+        match = _pattern.search(line)
+        if match is None:
+            raise ValueError(f"Cannot parse line: {line!r}")
+
+        min_count, max_count, character, password, *_ = match.groups()
+        return cls(int(min_count), int(max_count), character, password)
+
+    def is_valid_policy_1(self) -> bool:
+        count = self.password.count(self.character)
+        return self.min_count <= count <= self.max_count
+
+    def is_valid_policy_2(self) -> bool:
+        count = 0
+        for number in (self.min_count, self.max_count):
+            idx = number - 1
+            if 0 <= idx < len(self.password) and self.password[idx] == self.character:
+                count += 1
+
+        return count == 1
+
+
+def check_password_file(filename: str) -> None:
+    valid_count_1 = 0
+    valid_count_2 = 0
+    with open(filename, "rt", encoding="utf-8") as infile:
+        for line in infile:
+            record = Record.from_password_list_line(line)
+            if record.is_valid_policy_1():
+                valid_count_1 += 1
+            if record.is_valid_policy_2():
+                valid_count_2 += 1
+
+    print(f"Valid passwords: {valid_count_1} / {valid_count_2}")
+
+
+def main() -> None:
+    check_password_file(INPUT)
+
+
+if __name__ == "__main__":
+    main()
