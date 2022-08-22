d922ac99a4747d3cb8753cebb07b4093f1667c30
Michal Caban <cabanmichal@gmail.com>
2022-08-22 20:57:40 +0200

    day 14

--- aoc2020_14.py
+++ aoc2020_14.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """https://adventofcode.com/2020/day/14"""
+import itertools
 import re
 from typing import Dict, List, NamedTuple, Optional, cast
 
@@ -70,6 +71,42 @@ class Program:
                 bits.append(mask)
         return int("".join(bits), 2)
 
+    def execute_version_2(self) -> None:
+        self._memory = {}
+        for instruction in self._instructions:
+            if instruction.operation == "mask":
+                self._mask = cast(str, instruction.value)
+            elif instruction.operation == "mem":
+                for address in self.apply_mask_version_2(
+                    cast(int, instruction.address)
+                ):
+                    self._memory[address] = cast(int, instruction.value)
+            else:
+                raise ValueError(f"Incorrect instruction: {instruction!r}")
+
+    def apply_mask_version_2(self, address: int) -> List[int]:
+        bits = []
+        floating_bits = []
+        for idx, (mask, bit) in enumerate(
+            zip(self._mask, bin(address)[2:].zfill(self._width))
+        ):
+            if mask == "X":
+                floating_bits.append(idx)
+                bits.append("X")
+            elif mask == "1":
+                bits.append(mask)
+            else:
+                bits.append(bit)
+
+        combinations = itertools.product("01", repeat=len(floating_bits))
+        addresses: List[int] = []
+        for combination in combinations:
+            for bit_idx, bit_value in zip(floating_bits, combination):
+                bits[bit_idx] = bit_value
+            addresses.append(int("".join(bits), 2))
+
+        return addresses
+
     def dump_memory(self) -> Dict[int, int]:
         return self._memory.copy()
 
@@ -82,6 +119,11 @@ def main() -> None:
     print(
         f"Memory values sum part 1: {sum(program.dump_memory().values())}"
     )  # 17481577045893
+
+    program.execute_version_2()
+    print(
+        f"Memory values sum part 2: {sum(program.dump_memory().values())}"
+    )  # 4160009892257
 
 
 if __name__ == "__main__":
