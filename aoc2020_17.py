273d722203e1df46b1c75402b1421dfdb6cb5add
Michal Caban <cabanmichal@gmail.com>
2022-08-25 07:20:05 +0200

    day 17

--- aoc2020_17.py
+++ aoc2020_17.py
@@ -7,7 +7,7 @@ import itertools
 INPUT = "aoc2020_17_input.txt"
 ACTIVE = "#"
 T_SQUARE = tuple[int, int]
-T_CUBE = tuple[int, int, int]
+T_CUBE = tuple[int, ...]
 
 
 def load_input(input_file: str) -> tuple[set[T_SQUARE], tuple[int, int]]:
@@ -20,55 +20,69 @@ def load_input(input_file: str) -> tuple[set[T_SQUARE], tuple[int, int]]:
             for x, cube in enumerate(line):
                 if cube == ACTIVE:
                     active_cubes.add((x, y))
-    return active_cubes, (0, size)
+
+    return active_cubes, (-1, size + 1)
 
 
-def format_squares_as_cubes(squares: set[T_SQUARE]) -> set[T_CUBE]:
-    return {(x, y, 0) for x, y in squares}
+def format_squares_as_cubes(squares: set[T_SQUARE], dimension: int) -> set[T_CUBE]:
+    return {(x, y, *(0,) * (dimension - 2)) for x, y in squares}
 
 
-def adjacent_cubes(cube: T_CUBE) -> set[T_CUBE]:
-    cubes = set()
+def adjacent_cubes(cube: T_CUBE, dimension: int) -> set[T_CUBE]:
+    cubes: set[tuple[int, ...]] = set()
     differences = (-1, 0, 1)
-    x, y, z = cube
-    for dx, dy, dz in itertools.product(differences, repeat=3):
-        if dx == 0 and dy == 0 and dz == 0:
+    for coordinate_differences in itertools.product(differences, repeat=dimension):
+        if not any(coordinate_differences):  # all 0s => the cube itself
             continue
-        cubes.add((x + dx, y + dy, z + dz))
+        adjacent_cube = tuple(
+            coordinate + difference
+            for coordinate, difference in zip(cube, coordinate_differences)
+        )
+        cubes.add(adjacent_cube)
+
     return cubes
 
 
-def cycle_cubes(active_cubes: set[T_CUBE], range_: tuple[int, int]) -> set[T_CUBE]:
-    new_active_cubes = set()
+def cycle_cubes(
+    active_cubes: set[T_CUBE], range_: tuple[int, int], dimension: int
+) -> set[T_CUBE]:
+    new_active_cubes: set[tuple[int, ...]] = set()
     start, end = range_
-    start -= 1
-    end += 1
-    for x, y, z in itertools.product(range(start, end), repeat=3):
-        cube = (x, y, z)
-        active_neighbours = active_cubes & adjacent_cubes(cube)
+
+    for cube in itertools.product(range(start, end), repeat=dimension):
+        active_neighbours = active_cubes & adjacent_cubes(cube, dimension)
         if cube in active_cubes:
             if 2 <= len(active_neighbours) <= 3:
                 new_active_cubes.add(cube)
         elif len(active_neighbours) == 3:
             new_active_cubes.add(cube)
+
     return new_active_cubes
 
 
 def cycle_cubes_n_times(
-    active_cubes: set[T_CUBE], range_: tuple[int, int], n: int
+    active_cubes: set[T_CUBE], range_: tuple[int, int], n: int, dimension: int
 ) -> set[T_CUBE]:
     start, end = range_
     for _ in range(n):
-        active_cubes = cycle_cubes(active_cubes, (start, end))
+        active_cubes = cycle_cubes(active_cubes, (start, end), dimension)
         start -= 1
         end += 1
+
     return active_cubes
 
 
 def main() -> None:
     squares, range_ = load_input(INPUT)
-    active_cubes = len(cycle_cubes_n_times(format_squares_as_cubes(squares), range_, 6))
+    active_cubes = len(
+        cycle_cubes_n_times(format_squares_as_cubes(squares, 3), range_, 6, 3)
+    )
     print(f"Active cubes after 6th cycle: {active_cubes}")  # 319
+
+    active_cubes = len(
+        cycle_cubes_n_times(format_squares_as_cubes(squares, 4), range_, 6, 4)
+    )
+    print(f"Active cubes after 6th cycle: {active_cubes}")  # 2324
 
 
 if __name__ == "__main__":
